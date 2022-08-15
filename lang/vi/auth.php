d836c980cc50193085e9d03338e4433924583450
phuocnamevn <phuoc01tb@gmail.com>
2022-08-15 02:19:12 +0000

    me

--- lang/vi/auth.php
+++ lang/vi/auth.php
@@ -13,8 +13,8 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'password' => 'The provided password is incorrect.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'Các thông tin xác thực này không khớp với hồ sơ của chúng tôi.',
+    'password' => 'Mật khẩu được cung cấp không chính xác.',
+    'throttle' => 'Quá nhiều lần đăng nhập. Vui lòng thử lại sau: seconds giây.',
 
 ];
---
+++ lang/vi/messages.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'success' => 'Thành công',
+    'list_permission_group' => 'Danh sách nhóm quyền',
+    'create' => 'Tạo',
+    'edit' => 'Sửa',
+    'show' => 'Chi tiết',
+    'delete' => 'Xóa',
+    'back' => 'Quay lại',
+    'save' => 'Lưu',
+    'reset' => 'Đặt lại',
+    'name' => 'Tên',
+    'do_you_want_to_delete' => 'Bạn có muốn xóa không?',
+    'delete_success' => 'Xóa thành công',
+    'create_success' => 'Tạo thành công',
+    'edit_success' => 'Sửa thành công'
+];
